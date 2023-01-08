a64d5798902d61b5aa1037ed2f23457f2c6ff7ee
Amit <amitbhuse07@gmail.com>
2023-01-08 11:17:34 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package javaangers;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Amit Bhuse !");
+
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+package javaangers;
+
+public class Test {
+
+	public static void main(String[] args) {
+	 System.out.println(" Hey amit what are tou doing ! ");
+	 System.out.println(" Welcome to the world of Java Programing");
+
+	}
+
+}
